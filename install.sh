93ffad47e56488c8ec4f228dde0bfc91ead4794b
RABijl <r.a.bijl@hotmail.nl>
2021-12-20 22:24:40 +0000

    adds install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install script which creates symbolic links to the repo, 
+# allowing for git pulls without having to copy everything again.
+# comment out files you want to keep.
+# this script needs to be rerun if new files are added.
+
+execution_dir=$(pwd); 
+
+# bash configs
+ln -sfnv "$execution_dir/bash/.bashrc" ~/
+ln -sfnv "$execution_dir/bash/.bash_logout" ~/
+ln -sfnv "$execution_dir/bash/.bash_profile" ~/
+
+
+# kitty configs
+ln -sfnv "$execution_dir/kitty/" ~/.config/
